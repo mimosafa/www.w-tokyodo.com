d2baf50cec7f236a00d682efd877bad7892924ee
mimoto.t <mimoto@w-tokyodo.com>
2015-08-31 19:15:33 +0900

    Disable Emoji

--- www.w-tokyodo.dev/wstd/wp-content/plugins/workstoretokyodo/files/immutable-settings.php
+++ www.w-tokyodo.dev/wstd/wp-content/plugins/workstoretokyodo/files/immutable-settings.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Immutable Settings
+ * Immutable Settings & Hacks
  */
 
 add_action( 'after_setup_theme', 'wstd_immutable_settings' );
@@ -25,5 +25,28 @@ function wstd_immutable_settings() {
 	 * 固定ページに抜粋
 	 */
 	add_post_type_support( 'page', 'excerpt' );
+
+	/**
+	 * Disable Emoji
+	 * @see http://wordpress.stackexchange.com/questions/185577/disable-emojicons-introduced-with-wp-4-2
+	 */
+	add_action( 'init', function() {
+		// all actions related to emojis
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+		// filter to remove TinyMCE emojis
+		add_filter( 'tiny_mce_plugins', function( $plugins ) {
+			if ( is_array( $plugins ) )
+				return array_diff( $plugins, array( 'wpemoji' ) );
+			else
+				return array();
+		} );
+	} );
 
 }
